13ff992e54999814a01422850af30875de9f769a
developerdeepika <deepikadangi9060@gmail.com>
2023-03-27 15:09:32 +0530

    ruby-practice

---
+++ Hello.rb
@@ -0,0 +1,91 @@
+puts "geting started"
+puts "Hello World"
+# ===================================================================================
+
+puts "for loop -"
+i = "Deepika"
+
+for a in 1..5 do
+  puts i
+end
+# =============================================================================================
+
+puts "while loop -"
+
+x = 4
+while x >= 1
+  puts "Deepika"
+  x = x-1
+end
+
+# ==========================================================================================
+
+puts "do..while loop"
+
+loop do
+  puts "Deepika"
+  val = '7'
+  if val == '7'
+      break
+  end
+end
+# ===================================================================================
+
+puts "until loop"
+
+var = 7
+
+until var == 11 do
+  puts var * 10
+  var = var + 1
+end
+
+puts " for getting input from user"
+
+print "How old are you ? "
+age = gets.chomp
+
+# ============================================================================================
+
+puts "use of methods"
+
+# Here geeks is the method name
+def geeks
+ 
+  # statements to be displayed
+  puts "Welcome to GFG portal"
+   
+  # keyword to end method
+  end
+   
+  # calling of the method
+  geeks
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
